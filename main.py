20c79d61fb0cbb92b1b92057cb6691368f8ff9bd
Sharvin-M <smanjrekar2@ucmerced.edu>
2023-11-12 21:36:02 -0800

    debugging sqlite3 db implementation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect, g
+
 # from flask_mail import Mail, Message
 from flask_mail import Mail, Message
 from flask_sqlalchemy import SQLAlchemy
@@ -11,12 +12,10 @@ import os
 import smtplib
 
 
-
 # page routes
 app = Flask(__name__)
 SECRET_KEY = os.urandom(32)
-app.config['SECRET_KEY'] = SECRET_KEY
-
+app.config["SECRET_KEY"] = SECRET_KEY
 
 
 class ContactForm(FlaskForm):
@@ -27,40 +26,50 @@ class ContactForm(FlaskForm):
     submit = SubmitField("Send")
 
 
-@app.route("/", methods=['POST', 'GET'])
+@app.route("/", methods=["POST", "GET"])
 def index():
     return render_template("index.html")
+
 
 @app.route("/resume")
 def resume():
     return render_template("resume.html")
-        
+
+
 @app.route("/projects")
 def projects():
     return render_template("projects.html")
 
-@app.route("/contact", methods=['POST', 'GET'])
+
+@app.route("/contact", methods=["POST", "GET"])
 def get_contact():
     form = ContactForm()
-    if request.method == 'POST':
-        name =  request.form["name"]
+    if request.method == "POST":
+        name = request.form["name"]
         email = request.form["email"]
         subject = request.form["subject"]
         message = request.form["message"]
-        res = pd.DataFrame({'name':name, 'email':email, 'subject':subject ,'message':message}, index=[0])
+        res = pd.DataFrame(
+            {"name": name, "email": email, "subject": subject, "message": message},
+            index=[0],
+        )
         # res.to_csv('./contactUsMessage.csv')
 
-        con = sqlite3.connect('contactsPage.db') # create/connect to sqlite db and thene ccreate a cursor to execute cmds
+        con = sqlite3.connect(
+            "contactsPage.db"
+        )  # create/connect to sqlite db and thene ccreate a cursor to execute cmds
         cur = con.cursor()
-        cur.execute('''CREATE TABLE IF NOT EXISTS contactsPage
-            (sku text PRIMARY KEY, name text, email text, subject text, message text)''')
+        cur.execute(
+            """CREATE TABLE IF NOT EXISTS contactsPage
+            (sku text PRIMARY KEY, name text, email text, subject text, message text)"""
+        )
         cur.executemany("INSERT INTO contactsPage VALUES (?, ?, ?, ?)", res)
         cur.close()
         con.commit()
-        
-        return render_template('ThankYou.html')
+        db.connections.close_all()
+        return render_template("ThankYou.html")
     else:
-        return render_template('contact.html', form=form)
+        return render_template("contact.html", form=form)
 
 
 if __name__ == "__main__":
